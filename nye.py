094b03567bd251bc038eaad8e9d5d1ce63d5d948
HAM <mini_muz_11@hotmail.co.uk>
2020-01-02 16:47:35 +0000

    more refactoring - fix window bug

--- nye.py
+++ nye.py
@@ -4,18 +4,23 @@ import pyfiglet
 from colorama import Fore
 from subprocess import call
 
-msg = 'Happy New Year 2020'
+MSG = 'Happy New Year 2020'
+FRONT = 'slant'
 
 
 def clear():
     # check and make call for specific operating system
-    _ = call('clear' if os.name =='posix' else 'cls')
+    _ = call('clear' if os.name =='posix' else 'cls', shell=True)
 
 
-f = pyfiglet.Figlet(font='slant')
-
-clear()
-for col in [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.CYAN, Fore.YELLOW] * 2 + [Fore.WHITE]:
-    print(col + f.renderText(msg))
-    time.sleep(0.5)
+def print_nye_to_console():
+    f = pyfiglet.Figlet(font=FRONT)
     clear()
+    for col in [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.CYAN, Fore.YELLOW] * 2 + [Fore.WHITE]:
+        print(col + f.renderText(MSG))
+        time.sleep(0.5)
+        clear()
+
+
+if __name__ == '__main__':
+    print_nye_to_console()
