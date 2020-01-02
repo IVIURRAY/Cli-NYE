900bd91071b0f1a1f25a0a302a6e951e0cc50c5b
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-01-02 12:06:49 +0000

    pep8

--- nye.py
+++ nye.py
@@ -6,6 +6,7 @@ from subprocess import call
 
 msg = 'Happy New Year 2020'
 
+
 def clear():
     # check and make call for specific operating system
     _ = call('clear' if os.name =='posix' else 'cls')
